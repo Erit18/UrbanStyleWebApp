2b6639ec307dd61cfb7faa5b0fb1d4b1f66b55ff
DannitaSQ26 <dannaroxanne123@gmail.com>
2024-11-29 17:06:35 -0500

    db

--- src/main/java/com/mycompany/aplicativowebintegrador/config/DatabaseConfig.java
+++ src/main/java/com/mycompany/aplicativowebintegrador/config/DatabaseConfig.java
@@ -3,7 +3,7 @@ package com.mycompany.aplicativowebintegrador.config;
 public class DatabaseConfig {
     private static final String URL = "jdbc:mysql://localhost:3306/UrbanStyleDB?useSSL=false&serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASSWORD = "erits321123";
+    private static final String PASSWORD = "susanibar3";
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
 
     // Getters para acceder a la configuración
